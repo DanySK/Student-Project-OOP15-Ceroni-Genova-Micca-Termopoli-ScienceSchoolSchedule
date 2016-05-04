52b0b74dae98329919416ae91278902a192d0f93
massimiliano_micca <massimiliano_micca@localhost>
2016-05-04 14:22:17 +0200

    Aggiunta nuovamente classe professor, prima volta non aggiunta

---
+++ src/Model/Professor.java
@@ -0,0 +1,14 @@
+package Model;
+
+import java.util.Set;
+
+public interface Professor {
+
+    public Person getProf() ;
+    public void setProf(Person prof);
+    public Set<ListCourses> getCourses();
+
+    public void setCourses(Set<ListCourses> courses);
+
+    public void addCourse(ListCourses c);
+}
