e2ab2872a963cfcd8516c09ba21789a53f32ca57
galyagenova4 <galyagenova4@localhost>
2016-05-05 16:53:43 +0200

    Modifiche sulla Classe PanelTable. Aggiunti i nomi delle righe e colonne principali nella tablella

--- src/View/PanelTable.java
+++ src/View/PanelTable.java
@@ -1,13 +1,19 @@
 package View;
 
 import javax.swing.*;
+import javax.swing.table.*;
 import javax.swing.border.*;
+
+import Model.Days;
+import Model.Hours;
+import Model.ListRoom;
+
 import java.awt.*;
 
 public class PanelTable {
 	
 	private JPanel panelTable;
-	private TableGUI table;
+	private TableModel table;
 	private JTable mainTable;
 	private JScrollPane scroll;
 	
@@ -16,11 +22,26 @@ public class PanelTable {
 		this.panelTable = new JPanel(new BorderLayout());
 		this.panelTable.setBorder(new TitledBorder("Panel Table"));
 		this.table = new TableGUI();
+		
+		int i = 1;
+		for (Days days : Days.values()) {
+			this.table.setValueAt(days.getString(), i++, 0);
+			for (ListRoom room : ListRoom.values()) {
+				this.table.setValueAt(room.getValue(), i++, 0);
+			}
+		}
+		i = 1;
+		for (Hours hours : Hours.values()) {
+			this.table.setValueAt(hours.getValue(), 0, i++);
+		}
+		
 		this.mainTable = new JTable(table);
 		this.scroll = new JScrollPane(mainTable);
 		
+		mainTable.setTableHeader(null);
 		mainTable.setFillsViewportHeight(true);
 		this.panelTable.add(scroll);
+		
 	}
 	
 	public JPanel getPanelTable(){
--- src/View/TableGUI.java
+++ src/View/TableGUI.java
@@ -1,6 +1,12 @@
 package View;
 
+import javax.swing.JTable;
 import javax.swing.table.*;
+
+import Model.*;
+
+import java.awt.Dimension;
+import java.util.*;
 
 public class TableGUI extends AbstractTableModel{
 
@@ -9,11 +15,35 @@ public class TableGUI extends AbstractTableModel{
 	 */
 	private static final long serialVersionUID = -782099090803983602L;
 
-	private static int COLUMNS = 7;
-	private static int ROWS = 67;
+	private static int COLUMNS = Hours.values().length + 1;
+	private static int ROWS = Days.values().length*ListRoom.values().length;
+	
+	private Map<Dimension, Object> dataTable = new HashMap<>();
 	
 	public TableGUI() {
 		// TODO Auto-generated constructor stub
+		/*final Set<String> setHours = new TreeSet<>();
+		for (Hours hours : Hours.values()) {
+			setHours.add(hours.getValue());
+		}
+		
+		final Set<String> setHours = new TreeSet<>();
+		for (Hours hours : Hours.values()) {
+			setHours.add(hours.getValue());
+		}
+		this.table.setValueAt(null, 0, 0);
+		int i = 1;
+		for (String s : setHours){
+			this.table.setValueAt(s, 0, i++);
+		}
+		
+		System.out.println(setHours.toString());
+		for (Days days : Days.values()) {
+			this.dataTable.put(days.getString(), setHours);
+			for (ListRoom room : ListRoom.values()) {
+				this.dataTable.put(room.getValue(), new TreeSet<>());
+			}
+		}*/
 	}
 
 	@Override
@@ -31,12 +61,19 @@ public class TableGUI extends AbstractTableModel{
 	@Override // DA CONTROLLARE
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		// TODO Auto-generated method stub
-		return null;
+		return this.dataTable.get(new Dimension(rowIndex, columnIndex));
 	}
+	
+	public void setValueAt(Object data, int row, int col)
+    {
+        Dimension coord = new Dimension(row, col);
+        this.dataTable.put(coord, data);
+        fireTableCellUpdated(row, col);
+    }
 	
 	// DA CONTROLLARE
 	public boolean isCellEditable(final int row, final int col) {
-		return true;
+		return false;
 	}
 
 }
