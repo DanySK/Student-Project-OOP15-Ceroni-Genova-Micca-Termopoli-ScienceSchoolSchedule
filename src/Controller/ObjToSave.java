8daa75fe506b15ab4abeabfb861305f19d3ddbaf
massimiliano_micca <massimiliano_micca@localhost>
2016-05-11 16:14:05 +0200

    aggiunte classi objToSave modificata

---
+++ src/Controller/ObjToSave.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import Model.Professor;
+import Model.Room;
+
+public class ObjToSave implements ObjToSaveInterface {
+
+    private Set<Room> listRoom = new HashSet<>();
+    private Set<Professor> listProfessor = new HashSet<>();
+    private Set<Reservation> listReservation = new HashSet<>();
+
+    public ObjToSave(Set<Room> listRoom, Set<Professor> listProfessor, Set<Reservation> listReservation) {
+        this.listReservation = listReservation;
+        this.listProfessor = listProfessor;
+        this.listRoom = listRoom;
+
+    }
+
+    public Set<Room> getListRoom() {
+        return listRoom;
+    }
+
+    public void setListRoom(Set<Room> listRoom) {
+        this.listRoom = listRoom;
+    }
+
+    public Set<Professor> getListProfessor() {
+        return listProfessor;
+    }
+
+    public void setListProfessor(Set<Professor> listProfessor) {
+        this.listProfessor = listProfessor;
+    }
+
+    public Set<Reservation> getListReservation() {
+        return listReservation;
+    }
+
+    public void setListReservation(Set<Reservation> listReservation) {
+        this.listReservation = listReservation;
+    }
+    
+    public void clear(){
+        this.listProfessor.clear();
+        this.listReservation.clear();
+        this.listRoom.clear();
+    }
+
+}
---
+++ src/Controller/ObjToSaveInterface.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import java.util.Set;
+
+import Model.Professor;
+import Model.Room;
+
+public interface ObjToSaveInterface {
+
+    public Set<Room> getListRoom();
+
+    public void setListRoom(Set<Room> listRoom);
+
+    public Set<Professor> getListProfessor();
+
+    public void setListProfessor(Set<Professor> listProfessor);
+
+    public Set<Reservation> getListReservation();
+
+    public void setListReservation(Set<Reservation> listReservation);
+
+    public void clear();
+}
