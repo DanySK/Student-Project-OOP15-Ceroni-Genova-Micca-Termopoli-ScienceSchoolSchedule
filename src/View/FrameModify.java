334ad725ed920e2e2cf140def2ddac86653e5d6f
anna.termopoli <anna.termopoli@localhost>
2016-05-24 15:30:57 +0200

    creazione listener classe FrameModify

--- src/View/FrameModify.java
+++ src/View/FrameModify.java
@@ -13,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -20,122 +21,161 @@ import Controller.ControllerWorkers;
 import Model.Courses;
 
 public class FrameModify {
-	
-	private final JFrame frameModify= new JFrame("Acquisizione nuovo elemento");
+
+	private final static String EMPTYSTR = " ";
+
+	private final JFrame frameModify = new JFrame("Acquisizione nuovo elemento");
 	private ControllerWorkers controller = new ControllerWorkers();
-	
-	private final JButton prof= new JButton("Professore");
-	private final JButton room= new JButton("Aula");
+
+	private final JButton prof = new JButton("Professore");
+	private final JButton room = new JButton("Aula");
 	private final JComboBox<String> comboCourse1 = new JComboBox<String>();
 	private final JComboBox<String> comboCourse2 = new JComboBox<String>();
 	private final JComboBox<String> comboCourse3 = new JComboBox<String>();
-	
+
 	public FrameModify() {
 		this.frameModify.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frameModify.setVisible(true);
-        this.frameModify.setSize(400, 180);
-        this.frameModify.setResizable(false);
-        
-        final JPanel panelModify = new JPanel(new GridBagLayout());
-        final GridBagConstraints cnst= new GridBagConstraints();
-        cnst.gridy=0;
-        
-        cnst.fill= GridBagConstraints.HORIZONTAL;
-        panelModify.add(prof,cnst);
-        cnst.gridy++;
-        panelModify.add(room,cnst);
-        
-       prof.addActionListener(l->{
-    	   
-    	   this.frameModify.setVisible(false);
-    	   final JFrame frameProf=new JFrame();    	   
-    	   frameProf.setVisible(true);
-    	   frameProf.setTitle("Inserimento nuovo professore");
-    	   frameProf.setSize(800, 180);
-    	   frameProf.setResizable(false);
-    	   final JPanel profPanel=new JPanel(new GridBagLayout());
-    	   final GridBagConstraints cost= new GridBagConstraints();
-    	   final JLabel profLabel= new JLabel("Professore");
-    	   final JLabel cours1Label= new JLabel("Corso 1:");
-    	   final JLabel cours2Label= new JLabel("Corso 2:");
-    	   final JLabel cours3Label= new JLabel("Corso 3:");
-    	   
-    	   final JTextField insProf= new JTextField(15);
-   
-    	   
-    	   this.comboCourse1.addItem(" ");
-    	   for (Courses courses : this.controller.getCoursesFromFile()) {
-    		   this.comboCourse1.addItem( courses.getName());
+		this.frameModify.setVisible(true);
+		this.frameModify.setSize(400, 180);
+		this.frameModify.setResizable(false);
+
+		final JPanel panelModify = new JPanel(new GridBagLayout());
+		final GridBagConstraints cnst = new GridBagConstraints();
+		cnst.gridy = 0;
+
+		cnst.fill = GridBagConstraints.HORIZONTAL;
+		panelModify.add(prof, cnst);
+		cnst.gridy++;
+		panelModify.add(room, cnst);
+
+		prof.addActionListener(l -> {
+
+			this.frameModify.setVisible(false);
+			final JFrame frameProf = new JFrame();
+			frameProf.setVisible(true);
+			frameProf.setTitle("Inserimento nuovo professore");
+			frameProf.setSize(800, 180);
+			frameProf.setResizable(false);
+			final JPanel profPanel = new JPanel(new GridBagLayout());
+			final GridBagConstraints cost = new GridBagConstraints();
+			final JLabel profLabel = new JLabel("Professore");
+			final JLabel cours1Label = new JLabel("Corso 1:");
+			final JLabel cours2Label = new JLabel("Corso 2:");
+			final JLabel cours3Label = new JLabel("Corso 3:");
+
+			final JTextField insProf = new JTextField(15);
+
+			this.comboCourse1.addItem(EMPTYSTR);
+			for (Courses courses : this.controller.getCoursesFromFile()) {
+				this.comboCourse1.addItem(courses.getName());
+			}
+
+			this.comboCourse2.addItem(EMPTYSTR);
+			for (Courses courses : this.controller.getCoursesFromFile()) {
+				this.comboCourse2.addItem(courses.getName());
+			}
+
+			this.comboCourse3.addItem(EMPTYSTR);
+			for (Courses courses : this.controller.getCoursesFromFile()) {
+				this.comboCourse3.addItem(courses.getName());
+			}
+
+			this.comboCourse1.addActionListener(c -> {
+				this.showError();
+			});
+			this.comboCourse2.addActionListener(c -> {
+				this.showError();
+			});
+			this.comboCourse3.addActionListener(c -> {
+				this.showError();
+			});
+
+			cost.gridx = 0;
+			cost.gridy = 1;
+			cost.fill = GridBagConstraints.BOTH;
+			profPanel.add(profLabel, cost);
+			cost.gridx = 1;
+			profPanel.add(insProf, cost);
+			cost.gridy++;
+			cost.gridy++;
+			cost.gridx = 0;
+			profPanel.add(cours1Label, cost);
+			cost.gridx = 1;
+			profPanel.add(this.comboCourse1, cost);
+			cost.gridy++;
+			cost.gridx = 0;
+			profPanel.add(cours2Label, cost);
+			cost.gridx = 1;
+			profPanel.add(this.comboCourse2, cost);
+			cost.gridy++;
+			cost.gridx = 0;
+			profPanel.add(cours3Label, cost);
+			cost.gridx = 1;
+			profPanel.add(this.comboCourse3, cost);
+
+			final JButton save = new JButton("Salva");
+			final JPanel panelSave = new JPanel();
+			panelSave.add(save);
+			frameProf.add(profPanel);
+			frameProf.add(panelSave, BorderLayout.SOUTH);
+
+			save.addActionListener(e->{
+				
+				JOptionPane.showMessageDialog(null, "STAI SALVANDO IL PROFESSORE:   "+insProf.getText()+("\n \n") +
+						"STAI SALVANDO IL CORSO:\n"+this.comboCourse1.getSelectedItem()+("\n")+
+						this.comboCourse2.getSelectedItem()+("\n") + this.comboCourse3.getSelectedItem(),"Attenzione",
+						JOptionPane.ERROR_MESSAGE);
+				
+			});
+		});
+
+		room.addActionListener(l -> {
+
+			this.frameModify.setVisible(false);
+			final JFrame frameRoom = new JFrame();
+			frameRoom.setVisible(true);
+			frameRoom.setTitle("Inserimento nuovo professore");
+			frameRoom.setSize(400, 180);
+			frameRoom.setResizable(false);
+			final JPanel roomPanel = new JPanel(new GridBagLayout());
+			final JLabel roomLabel = new JLabel("Aula:");
+			final JTextField insRoom = new JTextField(15);
+			final JButton save = new JButton("Salva");
+			final JPanel panelSave = new JPanel();
+			panelSave.add(save);
+			frameRoom.add(roomPanel);
+			frameRoom.add(panelSave, BorderLayout.SOUTH);
+			save.addActionListener(e->{
+				JOptionPane.showMessageDialog(null, "STAI SALVANDO L'AULA:   "+insRoom.getText(),"Attenzione",
+						JOptionPane.ERROR_MESSAGE);
+				
+			});
+			roomPanel.add(roomLabel);
+			roomPanel.add(insRoom);
+			frameRoom.add(roomPanel);
+
+		});
+
+		frameModify.add(panelModify, BorderLayout.CENTER);
+		this.frameModify.setVisible(true);
+
+	}
+
+	private void showError() {
+
+		if (this.comboCourse1.getSelectedItem().toString().equals(this.comboCourse2.getSelectedItem().toString())
+				&& !this.comboCourse1.getSelectedItem().toString().equals(EMPTYSTR)
+				&& !this.comboCourse2.getSelectedItem().toString().equals(EMPTYSTR)
+				|| this.comboCourse1.getSelectedItem().toString().equals(this.comboCourse3.getSelectedItem().toString())
+						&& !this.comboCourse1.getSelectedItem().toString().equals(EMPTYSTR)
+						&& !this.comboCourse3.getSelectedItem().toString().equals(EMPTYSTR)
+				|| this.comboCourse2.getSelectedItem().toString().equals(this.comboCourse3.getSelectedItem().toString())
+						&& !this.comboCourse2.getSelectedItem().toString().equals(EMPTYSTR)
+						&& !this.comboCourse3.getSelectedItem().toString().equals(EMPTYSTR)) {
+			JOptionPane.showMessageDialog(null, "Hai inserito due o piu corsi uguali!", "Error",
+					JOptionPane.ERROR_MESSAGE);
 		}
-    	   
-    	   this.comboCourse2.addItem(" ");
-    	   for (Courses courses : this.controller.getCoursesFromFile()) {
-    		   this.comboCourse2.addItem( courses.getName());
-		}
-    	 
-    	   this.comboCourse3.addItem(" ");
-    	   for (Courses courses : this.controller.getCoursesFromFile()) {
-    		   this.comboCourse3.addItem( courses.getName());
-		}
-    	   
-    	   cost.gridx = 0;
-    	   cost.gridy = 1;
-           cost.fill = GridBagConstraints.BOTH;
-    	   profPanel.add(profLabel,cost);
-    	   cost.gridx = 1;
-    	   profPanel.add(insProf,cost);
-    	   cost.gridy++;
-    	   cost.gridy++;
-    	   cost.gridx = 0;
-    	   profPanel.add(cours1Label,cost);
-    	   cost.gridx = 1;
-    	   profPanel.add(this.comboCourse1,cost);
-    	   cost.gridy++;
-    	   cost.gridx = 0;
-    	   profPanel.add(cours2Label,cost);
-    	   cost.gridx = 1;
-    	   profPanel.add(this.comboCourse2,cost);
-    	   cost.gridy++;
-    	   cost.gridx = 0;
-    	   profPanel.add(cours3Label,cost);
-    	   cost.gridx = 1;
-    	   profPanel.add(this.comboCourse3, cost);
-    	   
-    	   final JButton save= new JButton("Salva");
-    	   final JPanel panelSave= new JPanel();
-    	   panelSave.add(save);
-    	   frameProf.add(profPanel);
-    	   frameProf.add(panelSave, BorderLayout.SOUTH);
-    	   
-       });
-        
-       room.addActionListener(l->{
-    	    
-    	   this.frameModify.setVisible(false);
-    	   final JFrame frameRoom=new JFrame();    	   
-    	   frameRoom.setVisible(true);
-    	   frameRoom.setTitle("Inserimento nuovo professore");
-    	   frameRoom.setSize(400, 180);
-    	   frameRoom.setResizable(false);
-    	   final JPanel roomPanel=new JPanel(new GridBagLayout());
-    	   final JLabel roomLabel= new JLabel("Aula:");
-    	   final JTextField insRoom= new JTextField(15);
-    	   final JButton save= new JButton("Salva");
-    	   final JPanel panelSave= new JPanel();
-    	   panelSave.add(save);
-    	   frameRoom.add(roomPanel);
-    	   frameRoom.add(panelSave, BorderLayout.SOUTH);
-    	   roomPanel.add(roomLabel);
-    	   roomPanel.add(insRoom);
-    	   
-    	   frameRoom.add(roomPanel);
-    	     
-       }); 
-        
-       frameModify.add(panelModify, BorderLayout.CENTER);
-       this.frameModify.setVisible(true);
-		
-		
+
 	}
 
 }
--- src/View/MainGUI.java
+++ src/View/MainGUI.java
@@ -11,34 +11,34 @@ import javax.swing.JScrollPane;
 
 public class MainGUI {
 
-    private JFrame frame = new JFrame("Science School Schedule");
-    private PanelButton panelButton = new PanelButton(this);
-    private PanelTable panelTable = new PanelTable();
-    private ComboBoxesViews combo = new ComboBoxesViews();
+	private JFrame frame = new JFrame("Science School Schedule");
+	private PanelButton panelButton = new PanelButton(this);
+	private PanelTable panelTable = new PanelTable();
+	private ComboBoxesViews combo = new ComboBoxesViews();
 
-    public MainGUI() {
+	public MainGUI() {
 
-        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        final int width = (int) screenSize.getWidth();
-        final int height = (int) screenSize.getHeight();
+		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		final int width = (int) screenSize.getWidth();
+		final int height = (int) screenSize.getHeight();
 
-        this.frame.setSize(width, height);
-        this.frame.add(panelButton.getPanelButton(), BorderLayout.WEST);
-        this.frame.add(panelTable.getPanelTable());
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setResizable(false);
-        this.frame.setVisible(true);
-        this.frame.repaint();
+		this.frame.setSize(width, height);
+		this.frame.add(panelButton.getPanelButton(), BorderLayout.WEST);
+		this.frame.add(panelTable.getPanelTable());
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setResizable(false);
+		this.frame.setVisible(true);
+		this.frame.repaint();
 
-    }
+	}
 
-    public void update(String s, Integer row, Integer colum) {
-        this.panelTable.Set(s, row, colum);
+	public void update(String s, Integer row, Integer colum) {
+		this.panelTable.Set(s, row, colum);
 
-    }
+	}
 
-    public static void main(String[] args) {
-        new MainGUI();
-    }
+	public static void main(String[] args) {
+		new MainGUI();
+	}
 
 }
