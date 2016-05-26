0f5ac7bcef75da68a7c763009b66e3eba3933e47
massimiliano_micca <massimiliano_micca@localhost>
2016-05-26 15:33:45 +0200

    divisa classe validate

--- src/Controller/validateError.java
+++ src/Controller/validateError.java
@@ -1,42 +1,16 @@
 package Controller;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import Model.Days;
 import Model.Type;
 
-public class validateController {
-
+public class validateError {
     private ControllerWorkers contWorkers = new ControllerWorkers();
-    private SaveController controllerSave = new SaveController();
+    private SaveController controllerSave = new SaveController(); 
     private Reservation cont;
-
-    public boolean validateWarning(Reservation cont) {
-        this.cont = cont;
-        Boolean check = true;
-        if (!checkHoursForDayProf()) {
-            check = false;
-            throw new IllegalArgumentException(); // 6 ore al giorno
-
-        } else if (!check4TimeWeekProfessor()) {
-            check = false;
-            throw new IllegalArgumentException(); // più di 4 giorni a settimana
-                                                  // prof
-
-        } else if (!check4TimeWeekStudent()) {
-            check = false;
-            throw new IllegalArgumentException(); // più di 4 giorni a settimana
-                                                  // stud
-
-        }
-
-        return check;
-    }
-
-    public boolean validateError(Reservation cont) {
+    
+    public boolean validateErrore(Reservation cont) {
         this.cont = cont;
         Boolean check = true;
         if(!check()){           // stessa cella non inseribile
@@ -74,6 +48,7 @@ public class validateController {
         return check;
 
     }
+    
     private boolean check() {
         boolean check = true;
         for (Reservation res : controllerSave.getObjToSave().getListReservation()) {
@@ -87,59 +62,7 @@ public class validateController {
         return check;
 
     }
-
-    private boolean checkHoursForDayProf() {
-        int i = 0;
-
-        for (Reservation reservation : this.contWorkers.getByDay(cont.getDay())) {
-            if (reservation.getPerson().toString().equals(cont.getPerson().toString())) {
-                i++;
-            }
-        }
-        return i <= 6 ? true : false;
-    }
-
-    private boolean check4TimeWeekProfessor() {
-        int i = 0;
-        Set<Integer> tempCount = new HashSet<>();
-        for (Reservation reservation : this.contWorkers.getByProfessor(cont.getPerson())) {
-            for (Days d : Days.values()) {
-                i = 0;
-                if (reservation.getDay().equals(d)) {
-                    i++;
-                }
-                if (i != 0) {
-                    tempCount.add(i);
-                }
-
-            }
-        }
-        return tempCount.size() <= 4 ? true : false;
-    }
-
-    private boolean check4TimeWeekStudent() {
-        int i = 0;
-        Set<Integer> tempCount = new HashSet<>();
-        List<Type> listType = new ArrayList<>();
-
-        for (Days d : Days.values()) {
-            i = 0;
-            for (Reservation res : this.contWorkers.getByDay(d)) {
-                listType.add(res.getCourse().getType());
-            }
-
-            if (listType.contains(cont.getCourse().getType())) {
-                i++;
-            }
-            if (i != 0) {
-                tempCount.add(i);
-            }
-
-        }
-
-        return tempCount.size() <= 4 ? true : false;
-    }
-
+    
     private Boolean checkCDLFirstYear() {
         boolean check = false;
         List<Type> listType = new ArrayList<>();
