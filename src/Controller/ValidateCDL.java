8760db9cec4ececce37b2487b8bb9998610021c2
massimiliano_micca <massimiliano_micca@localhost>
2016-05-30 18:11:49 +0200

    fix

--- src/Controller/ValidateCDL.java
+++ src/Controller/ValidateCDL.java
@@ -14,160 +14,192 @@ import Model.WarningException;
 public class ValidateCDL implements ValidateCDLInterface {
     private ControllerWorkers contWorkers = new ControllerWorkers();
     private Reservation cont;
-    
-    public void validate(Reservation res) throws WarningException, ErrorException{
+
+    public void validate(Reservation res) throws WarningException, ErrorException {
         this.cont = res;
-        
-        if (cont.getCourse().getType().equals(Type.FIRST_YEAR)) { // corso del 1°anno, corso non inseribile
+
+        if (cont.getCourse().getType().equals(Type.FIRST_YEAR)) {
+            // check first year course
             checkCDLFirstYear();
-        } else if (Type.getSecondYears().contains(cont.getCourse().getType())) { // corso del 2°anno, corso non inseribile
+        } else if (Type.getSecondYears().contains(cont.getCourse().getType())) {
+            // check second year course
             checkCDLSecondYear();
 
-        } else if (Type.getThirdYears().contains(cont.getCourse().getType())) { // corso del 3°anno, corso non inseribile
-           checkCDLThirdYear();
-           
-        } else if (cont.getCourse().getType().equals(Type.FOURTH_YEAR)) { // corso del 4°anno, corso non inseribile
+        } else if (Type.getThirdYears().contains(cont.getCourse().getType())) {
+            // check third year course
+            checkCDLThirdYear();
+
+        } else if (cont.getCourse().getType().equals(Type.FOURTH_YEAR)) {
+            // check fourth year course
             checkCDLFourthYear();
-     
-        } else if (Type.getFifthYears().contains(cont.getCourse().getType())) { // corso del 5°anno, corso non inseribile
-           checkCDLFifthYear();
+
+        } else if (Type.getFifthYears().contains(cont.getCourse().getType())) {
+            // check fifth year course
+            checkCDLFifthYear();
         }
     }
-        
-/**
- * 
- * @return
- * @throws WarningException
- * @throws ErrorException
- */
-private boolean checkCDLFirstYear() throws WarningException, ErrorException {
-    List<Type> listType = new ArrayList<>();
 
-    for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
-        if (cont.getHour().getValue().equals(res.getHour().getValue())) {
-            if (res.getCourse().getName().equals(cont.getCourse().getName())
-                    && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
-                    && !res.getPerson().toString().equals(cont.getPerson().toString())) {
+    /**
+     * this method check if in the same time slot are a equal type of course
+     * 
+     * @return
+     * @throws WarningException
+     * @throws ErrorException
+     */
+    private void checkCDLFirstYear() throws WarningException, ErrorException {
+        List<Type> listType = new ArrayList<>();
+
+        for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
+            if (cont.getHour().getValue().equals(res.getHour().getValue())) {
+                if (res.getCourse().getName().equals(cont.getCourse().getName())
+                        && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
+                        && !res.getPerson().toString().equals(cont.getPerson().toString())) {
                     throw new WarningException(
-                        "Un corso del primo anno è già inserito in questo periodo" + "\n" + "Inserirne un altro?"); //
+                            "Un corso del primo anno è già inserito in questo periodo" + "\n" + "Inserirne un altro?"); //
+                }
+                listType.add(res.getCourse().getType());
             }
-            listType.add(res.getCourse().getType());
+        }
+        if (listType.contains(Type.FIRST_YEAR)) {
+            throw new ErrorException(
+                    "Nella stessa fascia oraria è già presente un corso del primo anno, quindi non può essere sovrapposto");
         }
     }
-    if (listType.contains(Type.FIRST_YEAR)) {
-        throw new ErrorException(
-                "Nella stessa fascia oraria è già presente un corso del primo anno, quindi non può essere sovrapposto");
-    }
-    return true;
-}
 
-private boolean checkCDLSecondYear() throws WarningException, ErrorException {
-    List<Type> listType = new ArrayList<>();
+    /**
+     * 
+     * this method check if in the same time slot are a equal type of course
+     * 
+     * @return
+     * @throws WarningException
+     * @throws ErrorException
+     */
+    private void checkCDLSecondYear() throws WarningException, ErrorException {
+        List<Type> listType = new ArrayList<>();
 
-    for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
-        if ((cont.getHour().getValue()).equals(res.getHour().getValue())) {
-            if (res.getCourse().getName().equals(cont.getCourse().getName())
-                    && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
-                    && !res.getPerson().toString().equals(cont.getPerson().toString())) {
-                throw new WarningException("Un corso del secondo anno è già inserito in questo periodo" + "\n"
-                        + "Inserirne un altro?"); //
+        for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
+            if ((cont.getHour().getValue()).equals(res.getHour().getValue())) {
+                if (res.getCourse().getName().equals(cont.getCourse().getName())
+                        && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
+                        && !res.getPerson().toString().equals(cont.getPerson().toString())) {
+                    throw new WarningException("Un corso del secondo anno è già inserito in questo periodo" + "\n"
+                            + "Inserirne un altro?"); //
+                }
+                listType.add(res.getCourse().getType());
             }
-            listType.add(res.getCourse().getType());
+        }
+        if (listType.contains(Type.SECOND_YEAR)) {
+            throw new ErrorException(
+                    "Nella stessa fascia oraria è già presente un corso del secondo anno, quindi non può essere sovrapposto");
+        } else if (listType.contains(Type.SECOND_YEAR_ENG) && cont.getCourse().getType().equals(Type.SECOND_YEAR_SCI)) {
+
+        } else if (listType.contains(Type.SECOND_YEAR_SCI) && cont.getCourse().getType().equals(Type.SECOND_YEAR_ENG)) {
+
+        } else if (!listType.contains(cont.getCourse().getType())) {
+
+        } else {
+            throw new ErrorException(
+                    "Nella stessa fascia oraria è già presente un corso del secondo anno, quindi non può essere sovrapposto");
         }
     }
-    if(listType.contains(Type.SECOND_YEAR)){
-        throw new ErrorException(
-                "Nella stessa fascia oraria è già presente un corso del secondo anno, quindi non può essere sovrapposto");
-    }else if (listType.contains(Type.SECOND_YEAR_ENG) && cont.getCourse().getType().equals(Type.SECOND_YEAR_SCI)) {
-        return true;
-    } else if (listType.contains(Type.SECOND_YEAR_SCI) && cont.getCourse().getType().equals(Type.SECOND_YEAR_ENG)) {
-        return true; 
-    } else if(!listType.contains(cont.getCourse().getType())){
-        return true;
-    }else{
-        throw new ErrorException(
-                "Nella stessa fascia oraria è già presente un corso del secondo anno, quindi non può essere sovrapposto");
-    }
-}
 
-private boolean checkCDLThirdYear() throws WarningException, ErrorException {
+    /**
+     * 
+     * this method check if in the same time slot are a equal type of course
+     * 
+     * @return
+     * @throws WarningException
+     * @throws ErrorException
+     */
+    private void checkCDLThirdYear() throws WarningException, ErrorException {
 
-    List<Type> listType = new ArrayList<>();
+        List<Type> listType = new ArrayList<>();
 
-    for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
-        if ((cont.getHour().getValue()).equals(res.getHour().getValue())) {
-            if (res.getCourse().getName().equals(cont.getCourse().getName())
-                    && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
-                    && !res.getPerson().toString().equals(cont.getPerson().toString())) {
-                throw new WarningException(
-                        "Un corso del terzo anno è già inserito in questo periodo" + "\n" + "Inserirne un altro?"); //
+        for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
+            if ((cont.getHour().getValue()).equals(res.getHour().getValue())) {
+                if (res.getCourse().getName().equals(cont.getCourse().getName())
+                        && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
+                        && !res.getPerson().toString().equals(cont.getPerson().toString())) {
+                    throw new WarningException(
+                            "Un corso del terzo anno è già inserito in questo periodo" + "\n" + "Inserirne un altro?"); //
+                }
+                listType.add(res.getCourse().getType());
             }
-            listType.add(res.getCourse().getType());
+        }
+        if (listType.contains(Type.THIRD_YEAR)) {
+            throw new ErrorException(
+                    "Nella stessa fascia oraria è già presente un corso del terzo anno, quindi non può essere sovrapposto");
+
+        } else if (listType.contains(Type.THIRD_YEAR_ENG) && cont.getCourse().getType().equals(Type.THIRD_YEAR_SCI)) {
+
+        } else if (listType.contains(Type.THIRD_YEAR_SCI) && cont.getCourse().getType().equals(Type.THIRD_YEAR_ENG)) {
+
+        } else if (listType.contains(Type.THIRD_YEAR_OPT) && cont.getCourse().getType().equals(Type.THIRD_YEAR_OPT)) {
+
+        } else if (!listType.contains(cont.getCourse().getType())) {
+
+        } else {
+            throw new ErrorException(
+                    "Nella stessa fascia oraria è già presente un corso del terzo anno, quindi non può essere sovrapposto");
         }
     }
-     if(listType.contains(Type.THIRD_YEAR)){
-         throw new ErrorException(
-                 "Nella stessa fascia oraria è già presente un corso del terzo anno, quindi non può essere sovrapposto");
-     
-    }else if (listType.contains(Type.THIRD_YEAR_ENG) && cont.getCourse().getType().equals(Type.THIRD_YEAR_SCI)) {
-        return true;
-    } else if (listType.contains(Type.THIRD_YEAR_SCI) && cont.getCourse().getType().equals(Type.THIRD_YEAR_ENG)) {
-        return true;
-    } else if (listType.contains(Type.THIRD_YEAR_OPT) && cont.getCourse().getType().equals(Type.THIRD_YEAR_OPT)) {
-        return true;
-    }else if(!listType.contains(cont.getCourse().getType())){
-        return true;
-    } else {
-        throw new ErrorException(
-                "Nella stessa fascia oraria è già presente un corso del terzo anno, quindi non può essere sovrapposto");
-    }
-}
 
-private boolean checkCDLFourthYear() throws WarningException, ErrorException {
-  
-    List<Type> listType = new ArrayList<>();
-    for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
-        if ((cont.getHour().getValue()).equals(res.getHour().getValue())) {
-            if (res.getCourse().getName().equals(cont.getCourse().getName())
-                    && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
-                    && !res.getPerson().toString().equals(cont.getPerson().toString())) {
-                throw new WarningException(
-                        "Un corso del quarto anno è già inserito in questo periodo" + "\n" + "Inserirne un altro?"); //
-            }
-            listType.add(res.getCourse().getType());
+    /**
+     * 
+     * this method check if in the same time slot are a equal type of course
+     * 
+     * @return
+     * @throws WarningException
+     * @throws ErrorException
+     */
+    private void checkCDLFourthYear() throws WarningException, ErrorException {
 
-        }
-    }
-    if (listType.contains(Type.FOURTH_YEAR)) {
-        throw new ErrorException(
-                "Nella stessa fascia oraria è già presente un corso del quarto anno, quindi non può essere sovrapposto");
-    }
-
-    return true;
-}
-
-private boolean checkCDLFifthYear() throws WarningException, ErrorException {
-  
-    List<Type> listType = new ArrayList<>();
-
-    for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
-        if ((cont.getHour().getValue()).equals(res.getHour().getValue())) {
-            if (res.getCourse().getName().equals(cont.getCourse().getName())
-                    && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
-                    && !res.getPerson().toString().equals(cont.getPerson().toString())) {
-                throw new WarningException(
-                        "Un corso del quinto anno è già inserito in questo periodo" + "\n" + "Inserirne un altro?"); //
+        List<Type> listType = new ArrayList<>();
+        for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
+            if ((cont.getHour().getValue()).equals(res.getHour().getValue())) {
+                if (res.getCourse().getName().equals(cont.getCourse().getName())
+                        && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
+                        && !res.getPerson().toString().equals(cont.getPerson().toString())) {
+                    throw new WarningException(
+                            "Un corso del quarto anno è già inserito in questo periodo" + "\n" + "Inserirne un altro?"); //
+                }
+                listType.add(res.getCourse().getType());
 
             }
-            listType.add(res.getCourse().getType());
-
+        }
+        if (listType.contains(Type.FOURTH_YEAR)) {
+            throw new ErrorException(
+                    "Nella stessa fascia oraria è già presente un corso del quarto anno, quindi non può essere sovrapposto");
         }
     }
-    if (listType.contains(Type.FIFTH_YEAR) || listType.contains(Type.FIFTH_YEAR_OPT)) {
-        throw new ErrorException(
-                "Nella stessa fascia oraria è già presente un corso del quinto anno, quindi non può essere sovrapposto");
-    }
 
-    return true;
-}
+    /**
+     * this method check if in the same time slot are a equal type of course
+     * 
+     * @return
+     * @throws WarningException
+     * @throws ErrorException
+     */
+    private void checkCDLFifthYear() throws WarningException, ErrorException {
+
+        List<Type> listType = new ArrayList<>();
+
+        for (Reservation res : this.contWorkers.getByDay(cont.getDay())) {
+            if ((cont.getHour().getValue()).equals(res.getHour().getValue())) {
+                if (res.getCourse().getName().equals(cont.getCourse().getName())
+                        && !res.getRoom().getNameRoom().equals(cont.getRoom().getNameRoom())
+                        && !res.getPerson().toString().equals(cont.getPerson().toString())) {
+                    throw new WarningException(
+                            "Un corso del quinto anno è già inserito in questo periodo" + "\n" + "Inserirne un altro?"); 
+                }
+                listType.add(res.getCourse().getType());
+
+            }
+        }
+        if (listType.contains(Type.FIFTH_YEAR) || listType.contains(Type.FIFTH_YEAR_OPT)) {
+            throw new ErrorException(
+                    "Nella stessa fascia oraria è già presente un corso del quinto anno, quindi non può essere sovrapposto");
+        }
+
+    }
 }
--- src/View/ControllerGui.java
+++ src/View/ControllerGui.java
@@ -28,148 +28,148 @@ import Model.RoomImpl;
  */
 public class ControllerGui {
 
-	private final SaveControllerInterface cont = new SaveController();
-	private List<Reservation> setReservation = cont.getObjToSave().getListReservation();
-	private DefaultTableModel table = new TableGUI();
+    private final SaveControllerInterface cont = new SaveController();
+    private List<Reservation> setReservation = cont.getObjToSave().getListReservation();
+    private DefaultTableModel table = new TableGUI();
 
-	/**
-	 * this method draw all table
-	 * 
-	 */
-	private void drawTable() {
-		drawDefaultTable();
-		for (Reservation res : this.setReservation) {
-			this.table.setValueAt(res, this.getRow(res), this.getColum(res));
-			this.table.fireTableCellUpdated(this.getRow(res), this.getColum(res));
-		}
-		this.table.fireTableDataChanged();
-	}
+    /**
+     * this method draw all table
+     * 
+     */
+    private void drawTable() {
+        drawDefaultTable();
+        for (Reservation res : this.setReservation) {
+            this.table.setValueAt(res, this.getRow(res), this.getColum(res));
+            this.table.fireTableCellUpdated(this.getRow(res), this.getColum(res));
+        }
+        this.table.fireTableDataChanged();
+    }
 
-	/**
-	 * this method draw the main table by default
-	 * 
-	 */
-	private void drawDefaultTable() {
-		int i = 0;
-		for (Days days : Days.values()) {
-			int y = 1;
-			for (Hours hours : Hours.values()) {
+    /**
+     * this method draw the main table by default
+     * 
+     */
+    private void drawDefaultTable() {
+        int i = 0;
+        for (Days days : Days.values()) {
+            int y = 1;
+            for (Hours hours : Hours.values()) {
 
-				this.table.setValueAt(hours.getValue().toUpperCase(), i, y++);
+                this.table.setValueAt(hours.getValue().toUpperCase(), i, y++);
 
-			}
-			this.table.setValueAt(days.getString(), i++, 0);
-			for (Room room : this.cont.getObjToSave().getListRoom()) {
-				this.table.setValueAt(room.getNameRoom(), i++, 0);
-			}
-		}
-	}
+            }
+            this.table.setValueAt(days.getString(), i++, 0);
+            for (Room room : this.cont.getObjToSave().getListRoom()) {
+                this.table.setValueAt(room.getNameRoom(), i++, 0);
+            }
+        }
+    }
 
-	/**
-	 * for the main table
-	 * 
-	 * @return DefaultTableModel
-	 */
-	public DefaultTableModel getTable() {
+    /**
+     * for the main table
+     * 
+     * @return DefaultTableModel
+     */
+    public DefaultTableModel getTable() {
 
-		drawTable();
+        drawTable();
 
-		return this.table;
+        return this.table;
 
-	}
+    }
 
-	/**
-	 * for specific Reservation , returns his row
-	 * 
-	 * @param res
-	 * @return row
-	 */
+    /**
+     * for specific Reservation , returns his row
+     * 
+     * @param res
+     * @return row
+     */
 
-	public Integer getRow(Reservation res) {
-		int row = 0;
-		drawDefaultTable();
-		for (int r = 0; r < table.getRowCount(); r++) {
-			if (this.table.getValueAt(r, 0).equals(res.getDay().getString())) {
-				for (int a = r; a <= r + cont.getObjToSave().getListRoom().size(); a++) {
-					if ((this.table.getValueAt(a, 0).toString()).equals(res.getRoom().getNameRoom())) {
-						row = a;
-					}
+    public Integer getRow(Reservation res) {
+        int row = 0;
+        drawDefaultTable();
+        for (int r = 0; r < table.getRowCount(); r++) {
+            if (this.table.getValueAt(r, 0).equals(res.getDay().getString())) {
+                for (int a = r; a <= r + cont.getObjToSave().getListRoom().size(); a++) {
+                    if ((this.table.getValueAt(a, 0).toString()).equals(res.getRoom().getNameRoom())) {
+                        row = a;
+                    }
 
-				}
-			}
+                }
+            }
 
-		}
-		return row;
-	}
+        }
+        return row;
+    }
 
-	/**
-	 * for specific Reservation returns his column
-	 * 
-	 * @param res
-	 * @return column
-	 */
-	public Integer getColum(Reservation res) {
-		int colum = 0;
-		drawDefaultTable();
-		for (int c = 0; c < table.getColumnCount(); c++) {
-			if (this.table.getValueAt(0, c).toString().equals(res.getHour().getValue())) {
-				colum = c;
-			}
-		}
-		return colum;
-	}
+    /**
+     * for specific Reservation returns his column
+     * 
+     * @param res
+     * @return column
+     */
+    public Integer getColum(Reservation res) {
+        int colum = 0;
+        drawDefaultTable();
+        for (int c = 0; c < table.getColumnCount(); c++) {
+            if (this.table.getValueAt(0, c).toString().equals(res.getHour().getValue())) {
+                colum = c;
+            }
+        }
+        return colum;
+    }
 
-	/**
-	 * this method convert the object from Reservation to String
-	 * 
-	 * @param prof
-	 * @param corso
-	 * @param giorno
-	 * @param ora
-	 * @param stanza
-	 * @return new Reservation
-	 */
+    /**
+     * this method convert the String from JComboBOx to obj Reservation
+     * 
+     * @param prof
+     * @param corso
+     * @param giorno
+     * @param ora
+     * @param stanza
+     * @return new Reservation
+     */
 
-	public Reservation matchString(String prof, String corso, String giorno, String ora, String stanza) {
-		PersonImpl person = null;
-		CoursesImpl cours = null;
-		for (Professor p : cont.getObjToSave().getListProfessor()) {
+    public Reservation matchString(String prof, String corso, String giorno, String ora, String stanza) {
+        PersonImpl person = null;
+        CoursesImpl cours = null;
+        for (Professor p : cont.getObjToSave().getListProfessor()) {
 
-			if (p.getPerson().toString().equals(prof)) {
-				person = new PersonImpl(p.getPerson().getName(), p.getPerson().getSurname());
+            if (p.getPerson().toString().equals(prof)) {
+                person = new PersonImpl(p.getPerson().getName(), p.getPerson().getSurname());
 
-				for (CoursesImpl cous : p.getCourses()) {
-					if (cous.getName().equals(corso)) {
-						cours = new CoursesImpl(cous.getName(), cous.getType());
-					}
-				}
-			}
+                for (CoursesImpl cous : p.getCourses()) {
+                    if (cous.getName().equals(corso)) {
+                        cours = new CoursesImpl(cous.getName(), cous.getType());
+                    }
+                }
+            }
 
-		}
-		Days day = null;
-		for (Days d : Days.values()) {
-			if (d.getString().equals(giorno)) {
-				day = d;
-			}
-		}
-		Hours h = null;
-		for (Hours ho : Hours.values()) {
-			if (ho.getValue().equals(ora)) {
-				h = ho;
-			}
-		}
-		RoomImpl room = null;
-		for (RoomImpl r : cont.getObjToSave().getListRoom()) {
-			if (r.getNameRoom().equals(stanza)) {
-				room = r;
-			}
-		}
+        }
+        Days day = null;
+        for (Days d : Days.values()) {
+            if (d.getString().equals(giorno)) {
+                day = d;
+            }
+        }
+        Hours h = null;
+        for (Hours ho : Hours.values()) {
+            if (ho.getValue().equals(ora)) {
+                h = ho;
+            }
+        }
+        RoomImpl room = null;
+        for (RoomImpl r : cont.getObjToSave().getListRoom()) {
+            if (r.getNameRoom().equals(stanza)) {
+                room = r;
+            }
+        }
 
-		return new Reservation(person, cours, day, h, room);
-	}
+        return new Reservation(person, cours, day, h, room);
+    }
 
-	public SaveControllerInterface getCont() {
-		return this.cont;
-	}
+    public SaveControllerInterface getCont() {
+        return this.cont;
+    }
 
 }
