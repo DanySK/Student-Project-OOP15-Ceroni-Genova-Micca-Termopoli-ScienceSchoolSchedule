0b6c0c8ac46fe4c977ea3b338fc0aca1d78f9718
massimiliano_micca <massimiliano_micca@localhost>
2016-05-30 23:43:27 +0200

    fix

--- src/Controller/ValidateWarning.java
+++ src/Controller/ValidateWarning.java
@@ -1,100 +1,94 @@
 package Controller;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import Model.Days;
 import Model.ErrorException;
-import Model.Type;
 import Model.WarningException;
 
 public class ValidateWarning implements ValidateWarningInterface {
 
-	private ControllerWorkers contWorkers = new ControllerWorkers();
-	private Reservation cont;
+    private ControllerWorkers contWorkers = new ControllerWorkers();
+    private Reservation cont;
 
-	public void validateWARNING(Reservation cont) throws WarningException, ErrorException {
-		this.cont = cont;
+    public void validateWARNING(Reservation cont) throws WarningException, ErrorException {
+        this.cont = cont;
 
-		checkHoursForDayProf();// 6 ore al giorno
-		check4TimeWeekProfessor(); // più di 4 giorni a settimana,
-		check4TimeWeekStudent(); // più di 4 giorni a settimana
-	}
+        checkHoursForDayProf();// 6 ore al giorno
+        check4TimeWeekProfessor(); // più di 4 giorni a settimana,
+        check4TimeWeekStudent(); // più di 4 giorni a settimana
+    }
 
-	/**
-	 * this method check if professor work more of six hours in a day. if more
-	 * six hours make a warning error
-	 * 
-	 * @throws WarningException
-	 */
-	private void checkHoursForDayProf() throws WarningException {
-		int i = 0;
-		for (Reservation reservation : this.contWorkers.getByDay(cont.getDay())) {
-			if (reservation.getPerson().toString().equals(cont.getPerson().toString())) {
-				i++;
-			}
-		}
-		if (i >= 6) {
-			throw new WarningException("Il prof. svolge " + i + " ore di lezione" + "\n" + "Aggiungere la settima ora?");
-		}
-	}
+    /**
+     * this method check if professor work more of six hours in a day. if more
+     * six hours throw a warning error
+     * 
+     * @throws WarningException
+     */
+    private void checkHoursForDayProf() throws WarningException {
+        int i = 0;
+        for (Reservation reservation : this.contWorkers.getByDay(cont.getDay())) {
+            if (reservation.getPerson().toString().equals(cont.getPerson().toString())) {
+                i++;
+            }
+        }
+        if (i > 5) {
+            throw new WarningException("Il prof. svolge " + i + " ore di lezione" + "\n" + "Aggiungerne un'altra?");
+        }
+    }
 
-	/**
-	 * this method check if professor work more of 4 day in a week. if more 4
-	 * day make a warning error
-	 * 
-	 * @throws WarningException
-	 */
-	private void check4TimeWeekProfessor() throws WarningException {
-		int i = 0;
-		boolean contDay = false;
-		for (Days day : Days.values()) {
-			for (Reservation res : this.contWorkers.getByProfessor(cont.getPerson())) {
-				if(res.getDay().equals(day)){
-					contDay = true;
-				}
-			}
-			if(contDay){
-				contDay = false;
-				i++;
-			}
-		}
-		if(i >= 4){
-			throw new WarningException("Il prof. svolge " + i + " giorni di lezione" + "\n"
-					+ "Aggiungerlo anche nel quinto?");
-		}
-	}
+    /**
+     * this method check if professor work more of 4 day in a week. if more 4
+     * day throw  a warning error
+     * 
+     * @throws WarningException
+     */
+    private void check4TimeWeekProfessor() throws WarningException {
+        int i = 0;
+        boolean contDay = false;
+        for (Days day : Days.values()) {
+            for (Reservation res : this.contWorkers.getByProfessor(cont.getPerson())) {
+                if (res.getDay().equals(day)) {
+                    contDay = true;
+                }
+            }
+            if (contDay) {
+                contDay = false;
+                i++;
+            }
+        }
+        if (i == 4) {
+            throw new WarningException(
+                    "Il prof. svolge " + i + " giorni di lezione" + "\n" + "Aggiungerlo anche nel quinto?");
+        }
+    }
 
-	/**
-	 * this method check if students work more of 4 day in a week. if more 4 day
-	 * make a warning error
-	 * 
-	 * @throws WarningException
-	 */
-	private void check4TimeWeekStudent() throws WarningException {
+    /**
+     * this method check if students work more of 4 day in a week. if more 4 day
+     *             throw a warning error
+     * 
+     * @throws WarningException
+     */
+    private void check4TimeWeekStudent() throws WarningException {
 
-		int i = 0;
-		boolean isType = false;
-		for (Days day : Days.values()) {
-			for (Reservation res : this.contWorkers.getByDay(day)) {
-				if (this.cont.getCourse().getType().toString().equals(res.getCourse().getType().toString())) {
-					System.out.println(true);
-					isType = true;
-				}
-			}
-			if (isType) {
-				isType = false;
-				i++;
-				System.out.println(i);
-			}
-		}
-		if (i >= 4) {
-			throw new WarningException("Gli studenti di questo anno svolgono 4 giorni di lezione" + "\n"
-					+ "Aggiungerlo anche nel quinto?");
-		}
+        int i = 0;
+        boolean isType = false;
+        for (Days day : Days.values()) {
+            for (Reservation res : this.contWorkers.getByDay(day)) {
+                if (this.cont.getCourse().getType().toString().equals(res.getCourse().getType().toString())) {
+                    System.out.println(true);
+                    isType = true;
+                }
+            }
+            if (isType) {
+                isType = false;
+                i++;
+                System.out.println(i);
+            }
+        }
+        if (i == 4) {
+            throw new WarningException("Gli studenti di questo anno svolgono 4 giorni di lezione" + "\n"
+                    + "Aggiungerlo anche nel quinto?");
+        }
 
-	}
+    }
 
 }
